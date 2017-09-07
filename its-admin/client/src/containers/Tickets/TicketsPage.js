690c78505723b79dcd4e68a6744a4ae6f3b266d7
Josh MacDev <joshmacdev@victoroscosiMac.gateway>
2017-09-08 03:30:26 +1000

    Updated tickets page table to use search and pagination

--- its-admin/client/src/containers/Tickets/TicketsPage.js
+++ its-admin/client/src/containers/Tickets/TicketsPage.js
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
 import EntryHeader from "../../components/EntryHeader";
-import moment from "moment";
 import "../../stylesheets/TicketsPage.css";
 import faker from "faker";
+import {TicketsTableParent, TicketsTableControls, TicketsTable, TicketsTablePagination} from "../../components/TicketsTable";
 
 class TicketsPage extends Component {
 
     constructor(props){
         super(props);
+
+        // store all tickets here
+        this.tickets = this.getTickets();
+
+        // event binding
+        this.handleOnSearch = this.handleOnSearch.bind(this);
+        this.handleGoToPage = this.handleGoToPage.bind(this);
+        this.handleChangeItemsPerPage = this.handleChangeItemsPerPage.bind(this);
         
+        // use functions to compute properties
+        this.state = { currentPage: 1, itemsPerPage: 10, tickets: this.tickets };
+        this.state.firstItemIndex = () => (this.state.currentPage - 1) * this.state.itemsPerPage;
+        this.state.numPages = () =>Math.ceil(this.state.tickets.length / this.state.itemsPerPage);
+        this.state.displayedTickets = () => this.state.tickets.slice(this.state.firstItemIndex(), this.state.firstItemIndex() + this.state.itemsPerPage);
+    }
+
+    // randomly generate tickets using faker.js
+    getTickets(){
         const operating_system = ["Windows", "Mac", "Linux"];
-        const software_issue = [
-            "Google services setup", 
-            "Service accounts", 
-            "Storage",
-            "Cloud storage increase",
-            "Wifi Setup",
-            "Printing",
-            "Misconfigured software",
-            "other"
-        ];
+        const software_issue = [ "Google services setup", "Service accounts", "Storage", "Cloud storage increase", "Wifi Setup", "Printing", "Misconfigured software", "other"];
         let tickets = [];
+
         for(let i = 0; i < 1000; i++){
             tickets.push({
                 id: i + 1,
-                user: {
-                    fullname: faker.name.findName(), 
-                    email: faker.internet.email()
-                },
+                user: { fullname: faker.name.findName(), email: faker.internet.email() },
                 operating_system: operating_system[parseInt(Math.random() * operating_system.length, 10)],
                 software_issue: software_issue[parseInt(Math.random() * software_issue.length, 10)],
                 subject: faker.lorem.words(),
@@ -35,37 +41,26 @@ class TicketsPage extends Component {
                 status: "PENDING",
                 created_at: faker.date.recent()
             });
-        }
-        let currentPage = 0;
-        let itemsPerPage = 10;
-        let numPages = Math.ceil(tickets.length / itemsPerPage);
-        let firstItemIndex = 0;
-        let displayedTickets = tickets.slice(firstItemIndex, firstItemIndex + itemsPerPage)
+        };
 
-        let state = {
-            searchTerm: "",
-            tickets: tickets,
-            currentPage: 0,
-            itemsPerPage: 10,
-            firstItemIndex: currentPage * itemsPerPage,
-            numPages: function(){ return Math.ceil(state.tickets.length / state.itemsPerPage)},
-            displayedTickets: function(){ return state.tickets.slice(state.firstItemIndex, state.firstItemIndex + state.itemsPerPage)}
-        }
-
-        this.state = state;
+        return tickets.sort((a, b) => b.created_at - a.created_at);
     }
 
-    handleSearchButtonOnClick(event){
+    // handle search
+    handleOnSearch(event, keyword){
         event.preventDefault();
 
-        // much faster this way!
-        let searchTerm = document.getElementById("SearchInput").value.toLowerCase();
+        let searchTerm = keyword.replace(" ", "").toLowerCase();
 
+        // search with empty string will display all tickets
         if(searchTerm.split("").length === 0){
-            this.setState({displayedTickets: this.state.tickets});
+            this.setState({ currentPage: 1, tickets: this.tickets });
             return;
-        } else {
-            let displayedTickets = this.state.tickets.filter(ticket => {
+        } 
+        
+        // filter tickets
+        else {
+            let filteredTickets = this.tickets.filter(ticket => {
                 if(ticket.id.toString().indexOf(searchTerm) !== -1) return true;
                 if(ticket.user.fullname.replace(" ", "").toLowerCase().indexOf(searchTerm) !== -1) return true;
                 if(ticket.user.email.indexOf(searchTerm) !== -1) return true;
@@ -75,67 +70,64 @@ class TicketsPage extends Component {
                 if(ticket.status.replace(" ", "").toLowerCase().indexOf(searchTerm) !== -1) return true;
             });
 
-            this.setState({
-                displayedTickets: displayedTickets.slice(0, 10)
-            })
+            this.setState({ tickets: filteredTickets });
+        }
+
+    }
+
+    // sort by column
+    handleOnSelectHeader(event, header){
+        event.preventDefault();
+        console.log(header);
+    }
+
+    // handler when ticket is selected
+    handleOnSelectRow(event, ticket){
+        event.preventDefault();
+        console.log(ticket);
+    }
+
+    // handler for pagination
+    handleGoToPage(event, page){
+        console.log(page);
+        if(page > 0 && page <= this.state.numPages()){
+            this.setState({currentPage: page});
         }
     }
 
 
+    handleChangeItemsPerPage(event){
+        let itemsPerPage = Number(event.target.value);
+        this.setState({itemsPerPage: itemsPerPage});
+    }
+
+
+
     render() {
-
-        return (
-            <div id="TicketsPage">
-                <EntryHeader><h3>Tickets</h3></EntryHeader>
-                <div className="container">
-                    <div className="site-content">
-                        <div className="controls clearfix">
-                            <div className="controls-left-section">
-                                ({this.state.firstItemIndex + 1} - {this.state.itemsPerPage} of {this.state.tickets.length})
-                            </div>
-                            <div className="controls-right-section">
-                                <input type="text" id="SearchInput" name="search" className="form-control input-xs"/>
-                                <button className="btn btn-sm btn-primary" onClick={event => this.handleSearchButtonOnClick(event)}>Search</button>
-                            </div>
-                        </div>
-                        <div className="table-responsive">
-                            <table className="tickets-table">
-                                <thead>
-                                    <tr>
-                                        <th>Ticket</th>
-                                        <th>Status</th>
-                                        <th>Assigned to</th>
-                                        <th>Created</th>
-                                        <th>Last update</th>
-                                    </tr>
-                                </thead>
-                                <tbody>{this.state.displayedTickets().map(ticket => 
-                                    <tr key={ticket.id}>
-                                        <td>
-                                            <div className="ticket-summary">
-                                                <b className="ticket-subject">{ticket.subject}</b><br/>
-                                                <small className="ticket-meta">{ticket.software_issue} ({ticket.operating_system})</small>
-                                                <span className="ticket-id">#{ticket.id}</span>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <span className="ticket-status">{ticket.status.toString().toLowerCase()}</span>
-                                        </td>
-                                        <td><span className="ticket-assigned_to"></span></td>
-                                        <td><span className="ticket-created_at">{moment(ticket.created_at).format("DD/MM/YYYY").toString()}</span></td>
-                                        <td><span className="ticket-last_update"></span></td>
-                                    </tr>
-                                )}</tbody>
-                            </table>
-                        </div>
-
-                        <div>
-                            <button onClick={e => {this.setState({currentPage: this.state.currentPage + 1})}}>Next</button>
+        return <div id="TicketsPage">
+                    <EntryHeader><h3>Tickets</h3></EntryHeader>
+                    <div className="container">
+                        <div className="site-content">
+                            <TicketsTableParent>
+                                <TicketsTableControls 
+                                    current={this.state.firstItemIndex() + 1} 
+                                    itemsPerPage={this.state.itemsPerPage} 
+                                    numItems={this.state.tickets.length} 
+                                    changeItemsPerPage={this.handleChangeItemsPerPage}
+                                    onSearch={this.handleOnSearch} />
+                                <TicketsTable 
+                                    headers={["Tickets", "Status", "Assigned to", "Created", "Last update"]} 
+                                    onSelectHeader={this.handleOnSelectHeader} 
+                                    rows={this.state.displayedTickets()} 
+                                    onSelectRow={this.handleOnSelectRow} />
+                                <TicketsTablePagination 
+                                    currentPage={this.state.currentPage} 
+                                    numPages={this.state.numPages()} 
+                                    goToPage={this.handleGoToPage} />
+                            </TicketsTableParent>
                         </div>
                     </div>
-                </div>
-            </div>
-        );
+                </div>;
     }
 }
 
