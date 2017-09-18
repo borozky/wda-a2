bda2c2d9d1c7feb4f9044b0488c19178aca4ab4c
Josh MacDev <joshmacdev@victoroscosiMac.gateway>
2017-09-18 19:07:46 +1000

    Updated datatable search behavior, dealing with loading and empty items

--- its-admin/client/src/components/DataTable.js
+++ its-admin/client/src/components/DataTable.js
@@ -3,24 +3,49 @@ import React, { Component } from 'react';
 import Fuse from "fuse.js";
 import "../stylesheets/DataTable.css";
 
+const DataTableLoading = ({numCols}) => <tr><td colSpan={numCols} style={{textAlign: "center"}}>Loading...</td></tr>
+const DataTableNoItems = ({numCols}) => <tr><td colSpan={numCols} style={{textAlign: "center"}}>No items found.</td></tr>
+
+/**
+ * DataTable component
+ * 
+ * REQUIRED PROPS:
+ * - children(item, index) - children must be an anonymous function
+ * - data[]
+ * - columns[]
+ * - searchableItems[]
+ * 
+ * Optional Props
+ * - currentPage=1
+ * - itemsPerPage=10
+ * - onSelectRow()
+ * - onPaginate()
+ * - onSearch()
+ * - loading: false
+ */
 class DataTable extends Component {
 
     constructor(props){
         super(props);
         this.data = props.data; // cache data here
-        this.state = { currentPage: this.props.currentPage, itemsPerPage: this.props.itemsPerPage, data: [] };
+        this.state = { currentPage: this.props.currentPage, itemsPerPage: this.props.itemsPerPage, data: [], searchTerm: "" };
         this.firstItemIndex = this.firstItemIndex.bind(this);
         this.numPages = this.numPages.bind(this);
         this.displayedData = this.displayedData.bind(this);
         this.handleOnSearch = this.handleOnSearch.bind(this);
         this.handlePaginate = this.handlePaginate.bind(this);
+        this.handleOnChange = this.handleOnChange.bind(this);
     }
+
 
     // computed properties
     firstItemIndex(){ return (this.state.currentPage - 1) * this.state.itemsPerPage };
     numPages(){ return Math.ceil(this.state.data.length / this.state.itemsPerPage); }
     displayedData(){ return this.state.data.slice(this.firstItemIndex(), this.firstItemIndex() + this.state.itemsPerPage); }
 
+
+    // Use componentWillReceiveProps when working with fetching data outside this component
+    // eg. fetching from ajax
     componentWillReceiveProps(nextProps){
         this.data = nextProps.data;
         this.setState({ 
@@ -28,19 +53,22 @@ class DataTable extends Component {
         });
     }
 
-    // handle search - default search mechanism is provided by Fuse.js
-    handleOnSearch(event, keyword){
-        event.preventDefault();
 
+    // handle search functionality via Fuse.js
+    handleOnSearch(event){
+        event.preventDefault();
+        const keyword = document.getElementById("DatatableSearch").value.toString();
         if (keyword.trim().length === 0) {
-            this.setState({ currentPage: 1, data: this.data });
+            this.setState({ currentPage: 1, data: this.data, searchTerm: keyword });
         } else {
             this.setState({
                 data: this.props.onSearch(event, keyword, this.props.searchableItems), 
-                currentPage: 1
+                currentPage: 1,
+                searchTerm: keyword
             });
         }
     }
+
 
     // handle pagination
     handlePaginate(event, page){
@@ -51,22 +79,43 @@ class DataTable extends Component {
         }
     }
 
+
+    handleOnChange(event){
+        event.preventDefault();
+        let value = "";
+        if(event.target.tagName === "INPUT"){
+            value = event.target.value;
+        }
+        if(value.trim().length === 0){
+            this.setState({data: this.data});  // display original data when search box is empty
+        }
+    }
+
+
     render() {
-        let label = `${this.firstItemIndex()} - ${this.firstItemIndex() + this.state.itemsPerPage - 1} of ${this.state.data.length}`;
+
+        // handles the num of results label
+        // eg "1 - 10 of 1000 results"
+        let label = `${this.firstItemIndex() + 1} - ${this.firstItemIndex() + this.state.itemsPerPage} of ${this.state.data.length}`;
         if(this.props.data.length === 0){
             label = "0";
         } else if(this.firstItemIndex() + this.state.itemsPerPage > this.props.data.length){
             label = `${this.firstItemIndex()} - ${this.props.data.length} of ${this.props.data.length}`;
         }
 
+        const {searchTerm, currentPage} = this.state;
+        const {columns, children} = this.props;
+        const items = this.state.data.length > 0 ? 
+                        this.displayedData().map((item, index) => children(item, index)) : 
+                        <DataTableNoItems numCols={columns.length} />;
+
         return (
-            <div className="datatable-wrapper">
+            <div className="datatable-wrapper" style={this.props.style}>
                 <div className="datatable-controls">
                     <div className="datatable-control-left">{label} results</div>
                     <div className="datatable-search">
-                        <form onSubmit={e => {this.handleOnSearch(e, document.getElementById("DatatableSearch").value)}}>
-                            <input type="search" id="DatatableSearch"/>
-                            <button type="submit">Search</button>
+                        <form onSubmit={this.handleOnSearch.bind(this)}>
+                            <input type="search" id="DatatableSearch" placeholder="Search" defaultValue={searchTerm} onChange={this.handleOnChange}/>
                         </form>
                     </div>
                 </div>
@@ -74,18 +123,16 @@ class DataTable extends Component {
                     <table className="datatable">
                         <thead>
                             <tr>
-                                {this.props.columns.map((column, index) => <th key={index}>{column.toString()}</th>)}
+                                {columns.map((column, index) => <th key={index}>{column.toString()}</th>)}
                             </tr>
                         </thead>
-                        <tbody>
-                            {this.displayedData().map((item, index) => this.props.children(item, index))}
-                        </tbody>
+                        <tbody>{ this.props.loading ? <DataTableLoading numCols={columns.length}/> : items }</tbody>
                     </table>
                 </div>
                 <div className="datatable-pagination">
                     <button className="datatable-first" onClick={e => {this.handlePaginate(e, 1)}}>First</button>
-                    <button className="datatable-previous" onClick={e => {this.handlePaginate(e, this.state.currentPage - 1)}}>Previous</button>
-                    <button className="datatable-next" onClick={e => {this.handlePaginate(e, this.state.currentPage + 1)}}>Next</button>
+                    <button className="datatable-previous" onClick={e => {this.handlePaginate(e, currentPage - 1)}}>Previous</button>
+                    <button className="datatable-next" onClick={e => {this.handlePaginate(e, currentPage + 1)}}>Next</button>
                     <button className="datatable-last" onClick={e => {this.handlePaginate(e, this.numPages())}}>Last</button>
                 </div>
             </div>
@@ -93,9 +140,12 @@ class DataTable extends Component {
     }
 }
 
+
 DataTable.defaultProps = {
     currentPage: 1,
     itemsPerPage: 10,
+    style: {},
+    loading: false,
     onSearch: function(event, keyword, searchableItems){
         const fuse = new Fuse(this.data, {
             shouldSort: true,
