3019df9d26d37af06238c12a044e2508e4451554
Josh MacDev <joshmacdev@victoroscosiMac.gateway>
2017-09-18 19:14:03 +1000

    ticket page now reuse DataTable component

--- its-admin/client/src/containers/Tickets/TicketsPage.js
+++ its-admin/client/src/containers/Tickets/TicketsPage.js
@@ -1,137 +1,62 @@
 import React, { Component } from 'react';
 import EntryHeader from "../../components/EntryHeader";
-import "../../stylesheets/TicketsPage.css";
 import faker from "faker";
-import {TicketsTableParent, TicketsTableControls, TicketsTable, TicketsTablePagination} from "../../components/TicketsTable";
 import { Redirect } from "react-router-dom";
+import DataTable from "../../components/DataTable";
+import {connect} from "react-redux";
+import moment from "moment";
+import * as TicketActions from "../../actions/TicketActions";
 
 class TicketsPage extends Component {
 
-    constructor(props){
-        super(props);
-
-        // store all tickets here
-        this.tickets = this.getTickets();
-
-        // event binding
-        this.handleOnSearch = this.handleOnSearch.bind(this);
-        this.handleGoToPage = this.handleGoToPage.bind(this);
-        this.handleOnSelectRow = this.handleOnSelectRow.bind(this);
-        this.handleChangeItemsPerPage = this.handleChangeItemsPerPage.bind(this);
-        
-        // use functions to compute properties
-        this.state = { currentPage: 1, itemsPerPage: 10, tickets: this.tickets };
-        this.state.firstItemIndex = () => (this.state.currentPage - 1) * this.state.itemsPerPage;
-        this.state.numPages = () =>Math.ceil(this.state.tickets.length / this.state.itemsPerPage);
-        this.state.displayedTickets = () => this.state.tickets.slice(this.state.firstItemIndex(), this.state.firstItemIndex() + this.state.itemsPerPage);
+    componentDidMount(){
+        this.props.getAllTickets();
     }
 
-    // randomly generate tickets using faker.js
-    getTickets(){
-        const operating_system = ["Windows", "Mac", "Linux"];
-        const software_issue = [ "Google services setup", "Service accounts", "Storage", "Cloud storage increase", "Wifi Setup", "Printing", "Misconfigured software", "other"];
-        let tickets = [];
+    render(){
+        const {tickets, searchableItems, columns, loadingTickets} = this.props;
 
-        for(let i = 0; i < 1000; i++){
-            tickets.push({
-                id: i + 1,
-                user: { fullname: faker.name.findName(), email: faker.internet.email() },
-                operating_system: operating_system[parseInt(Math.random() * operating_system.length, 10)],
-                software_issue: software_issue[parseInt(Math.random() * software_issue.length, 10)],
-                subject: faker.lorem.words(),
-                details: faker.lorem.text(),
-                status: "PENDING",
-                created_at: faker.date.recent()
-            });
-        };
-
-        return tickets.sort((a, b) => b.created_at - a.created_at);
-    }
-
-    // handle search
-    handleOnSearch(event, keyword){
-        event.preventDefault();
-
-        let searchTerm = keyword.replace(" ", "").toLowerCase();
-
-        // search with empty string will display all tickets
-        if(searchTerm.split("").length === 0){
-            this.setState({ currentPage: 1, tickets: this.tickets });
-            return;
-        } 
-        
-        // filter tickets
-        else {
-            let filteredTickets = this.tickets.filter(ticket => {
-                if(ticket.id.toString().indexOf(searchTerm) !== -1) return true;
-                if(ticket.user.fullname.replace(" ", "").toLowerCase().indexOf(searchTerm) !== -1) return true;
-                if(ticket.user.email.indexOf(searchTerm) !== -1) return true;
-                if(ticket.operating_system.replace(" ", "").toLowerCase().indexOf(searchTerm) !== -1) return true;
-                if(ticket.software_issue.replace(" ", "").toLowerCase().indexOf(searchTerm) !== -1) return true;
-                if(ticket.subject.replace(" ", "").toLowerCase().indexOf(searchTerm) !== -1) return true;
-                if(ticket.status.replace(" ", "").toLowerCase().indexOf(searchTerm) !== -1) return true;
-            });
-
-            this.setState({ tickets: filteredTickets });
-        }
-
-    }
-
-    // sort by column
-    handleOnSelectHeader(event, header){
-        event.preventDefault();
-        console.log(header);
-    }
-
-    // handler when ticket is selected
-    handleOnSelectRow(event, ticket){
-        event.preventDefault();
-        console.log(ticket);
-        this.props.history.push({
-            pathname: `/tickets/${ticket.id}`,
-            state: { ticket: ticket }
-        });
-    }
-
-    // handler for pagination
-    handleGoToPage(event, page){
-        console.log(page);
-        if(page > 0 && page <= this.state.numPages()){
-            this.setState({currentPage: page});
-        }
-    }
-
-    handleChangeItemsPerPage(event){
-        let itemsPerPage = Number(event.target.value);
-        this.setState({itemsPerPage: itemsPerPage, currentPage: 1});
-    }
-
-    render() {
         return <div id="TicketsPage">
-                    <EntryHeader><h3>Tickets</h3></EntryHeader>
-                    <div className="container">
-                        <div className="site-content">
-                            <TicketsTableParent>
-                                <TicketsTableControls 
-                                    current={this.state.firstItemIndex() + 1} 
-                                    itemsPerPage={this.state.itemsPerPage} 
-                                    numItems={this.state.tickets.length} 
-                                    changeItemsPerPage={this.handleChangeItemsPerPage}
-                                    onSearch={this.handleOnSearch} />
-                                <TicketsTable 
-                                    headers={["Tickets", "Status", "Assigned to", "Created", "Last update"]} 
-                                    onSelectHeader={this.handleOnSelectHeader} 
-                                    rows={this.state.displayedTickets()} 
-                                    onSelectRow={this.handleOnSelectRow} />
-                                <TicketsTablePagination 
-                                    currentPage={this.state.currentPage} 
-                                    numPages={this.state.numPages()} 
-                                    goToPage={this.handleGoToPage} />
-                            </TicketsTableParent>
-                        </div>
-                    </div>
-                </div>;
+            <EntryHeader>
+                <h3>All Tickets</h3>
+            </EntryHeader>
+            <div className="site-content">
+                <div className="container">
+                    <DataTable loading={loadingTickets} data={tickets} searchableItems={searchableItems} columns={columns} style={{maxWidth: "100%"}}>{(ticket, index) => 
+                        <tr>
+                            <td>
+                                <div className="ticket-summary">
+                                    <b className="ticket-subject">{ticket.subject}</b><br/>
+                                    <small className="ticket-meta">{ticket.software_issue} ({ticket.operating_system})</small>
+                                    <span className="ticket-id">#{ticket.id}</span>
+                                </div>
+                            </td>
+                            <td><span className="ticket-assigned_to"></span></td>
+                            <td><span className="ticket-created_at">{moment(ticket.created_at).format("DD/MM/YYYY").toString()}</span></td>
+                        </tr>
+                    }</DataTable>
+                </div>
+            </div>
+        </div>
+    }
+
+}
+
+const mapStateToProps = (state) => {
+    return {
+        tickets: state.tickets.data,
+        searchableItems: ["id", "subject", "software_issue", "operating_system"],
+        columns: ["Tickets", "Assigned to", "Created"],
+        loadingTickets: state.tickets.loading
     }
 }
 
-export default TicketsPage;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getAllTickets: function(){
+            dispatch(TicketActions.getAllTickets());
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TicketsPage);
