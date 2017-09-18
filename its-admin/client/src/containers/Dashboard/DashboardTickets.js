82f5275b78b5d39234a28a934cd80ddb063ac886
Josh MacDev <joshmacdev@victoroscosiMac.gateway>
2017-09-18 19:12:59 +1000

    DashboardTicketRow component moved inside DashboardTickets

--- its-admin/client/src/containers/Dashboard/DashboardTickets.js
+++ its-admin/client/src/containers/Dashboard/DashboardTickets.js
@@ -1,29 +1,42 @@
 import React, { Component } from 'react';
 import DataTable, { DataTableRow } from "../../components/DataTable";
-import DashboardTicketRow from "./DashboardTicketRow";
 import {connect} from "react-redux";
+import Fuse from "fuse.js";
+import moment from "moment";
+
+const DashboardTicketRow = ({onSelectRow, ticket = {}, active = false}) => {
+    const {subject, software_issue, operating_system, id, created_at} = ticket;
+    return (
+        <tr onClick={e => onSelectRow(e, ticket)} className={ active ? "selected" : ""}>
+            <td>
+                <div className="ticket-summary">
+                    <b className="ticket-subject">{subject}</b><br/>
+                    <small className="ticket-meta">{software_issue} ({operating_system})</small>
+                    <span className="ticket-id">#{id}</span>
+                </div>
+            </td>
+            <td><span className="ticket-assigned_to"></span></td>
+            <td><span className="ticket-created_at">{moment(created_at).format("DD/MM/YYYY").toString()}</span></td>
+        </tr>
+    );
+};
 
 class DashboardTickets extends Component {
     render() {
-        console.log("TICKETS", this.props.data);
-        let props = this.props;
+        const {tickets, onSelectRow, columns, searchableItems, selectedTicket} = this.props;
         return (
-            <DataTable 
-                data={props.data} 
-                onSelectRow={props.onSelectRow} 
-                columns={["Tickets", "Assigned to", "Created"]} 
-                searchableItems={["id", "subject", "software_issue", "operating_system"]}>{ (ticket, index) => 
-                <DashboardTicketRow {...props} key={index} ticket={ticket} active={(props.selectedTicket && props.selectedTicket.id === ticket.id)}/>
+            <DataTable {...this.props} data={tickets}>{ (ticket, index) => 
+                <DashboardTicketRow {...this.props} key={index} ticket={ticket} active={(selectedTicket && selectedTicket.id === ticket.id)}/>
             }</DataTable>
         );
     }
-
-
 }
 
 const mapStateToProps = (state) => {
     return {
-        data: state.tickets.data
+        tickets: state.tickets.data,
+        searchableItems: ["id", "subject", "software_issue", "operating_system"],
+        columns: ["Tickets", "Assigned to", "Created"]
     }
 }
 
