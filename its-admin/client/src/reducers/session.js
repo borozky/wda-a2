caaf47f85cdec74762b122aa1e45a84e6dbfdb4b
Josh MacDev <joshmacdev@victoroscosiMac.gateway>
2017-09-28 00:19:56 +1000

    reducers for assigning roles for new users

--- its-admin/client/src/reducers/session.js
+++ its-admin/client/src/reducers/session.js
@@ -1,9 +1,11 @@
 import {Actions} from "../helpers/Constants";
 import * as SessionActions from "../actions/SessionActions";
+import * as StaffActions from "../actions/StaffActions";
 
 const initialSession = {
     currentUser: null,
-    loggingIn: false
+    loggingIn: false,
+    assigningRole: false
 }
 
 export default function session(state = initialSession, action){
@@ -25,6 +27,36 @@ export default function session(state = initialSession, action){
                 ...state,
                 currentUser: null,
                 loggingIn: false
+            }
+        case SessionActions.ASSIGNING_NEW_USER_ROLE:
+            return {
+                ...state,
+                assigningRole: true
+            };
+        case SessionActions.NEW_USER_ROLE_ASSIGNED:
+            return {
+                ...state,
+                assigningRole: false,
+                currentUser: {
+                    ...state.currentUser,
+                    role: action.payload
+                }
+            }
+        case StaffActions.GETTING_STAFF_ROLE:
+            if (state.currentUser == null) {
+                return state;
+            }
+            if (typeof state.currentUser.role == "undefined") {
+                return state;
+            }
+            if (state.currentUser.uid == action.payload.uid) {
+                return {
+                    ...state,
+                    currentUser: {
+                        ...state.currentUser,
+                        role: action.payload.role
+                    }
+                }
             }
         default:
             return state;
