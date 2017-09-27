c2e39666584af7a016d82c2dd9f068d64cc5a6a9
Josh MacDev <joshmacdev@victoroscosiMac.gateway>
2017-09-28 00:13:28 +1000

    added the actions for new staff members to choose role they want

--- its-admin/client/src/actions/StaffActions.js
+++ its-admin/client/src/actions/StaffActions.js
@@ -1,11 +1,38 @@
-export const GET_ALL_STAFF = "GET_ALL_STAFF";
-export const getAllStaff = () => (dispatch, getState) => {
-    const staff = {
-        data: []
-    }
+import { ref } from "../client";
 
-    dispatch({ 
-        type: GET_ALL_STAFF,
-        payload: staff
+export const GETTING_ALL_STAFF = "GETTING_ALL_STAFF";
+export const STAFF_RETRIEVED = "STAFF_RETRIEVED";
+export const GETTING_STAFF_ROLE = "GETTING_STAFF_ROLE";
+export const STAFF_ROLE_RETRIEVED = "STAFF_ROLE_RETRIEVED";
+export const STAFF_ROLE_NOT_IDENTIFIED = "STAFF_ROLE_NOT_IDENTIFIED";
+
+export const getAllStaff = () => (dispatch, getState) => {
+    dispatch({
+        type: GETTING_ALL_STAFF
+    });
+    ref.child("staff").once("value").then(function(snapshot){
+        console.log(snapshot.val());
+
+        dispatch({ 
+            type: STAFF_RETRIEVED,
+            payload: snapshot.val()
+        });
+    });
+}
+
+export const getStaffRole = (userID) => (dispatch, getState) => {
+    dispatch({type: GETTING_STAFF_ROLE});
+
+    ref.child("staff/" + userID + "/role").once("value").then(function(snapshot){
+        console.log("ROLE", snapshot.val());
+        dispatch({
+            type: STAFF_ROLE_RETRIEVED,
+            payload: {
+                uid: userID,
+                role: snapshot.val()
+            }
+        })
+    }).catch(function(error){
+        dispatch({ type: STAFF_ROLE_NOT_IDENTIFIED });
     });
 }
