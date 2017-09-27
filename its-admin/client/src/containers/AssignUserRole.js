c1828d4ff849ccff35b8be1392659642fc1d3b48
Josh MacDev <joshmacdev@victoroscosiMac.gateway>
2017-09-28 00:15:11 +1000

    page for choosing role

---
+++ its-admin/client/src/containers/AssignUserRole.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AssignUserRole extends Component {
+    render() {
+        return (
+            <div className="assign-user-role">
+                
+            </div>
+        );
+    }
+}
+
+export default AssignUserRole;
