6b3b447ce423258ee7bfcd0a95f3e48eea891d0b
Josh MacDev <joshmacdev@victoroscosiMac.gateway>
2017-09-28 00:23:59 +1000

    getting comments from laravel server with express

--- its-admin/server/routes/comments.js
+++ its-admin/server/routes/comments.js
@@ -3,12 +3,26 @@ var axios = require("axios");
 var router = express.Router();
 
 const COMMENTS_DATASOURCE_URL = `${process.env.DATASOURCE_URL}api/comments`;
+
 router.get('/', function(req, res, next) {
     axios.get(COMMENTS_DATASOURCE_URL).then(response => {
         return res.json(response.data);
     })
     .catch(error => {
-        return res.status(404);
+        let status = error.response.status || 404;
+        let data = error.response.data || [];
+        return res.status(status).json(data);
+    });
+});
+
+router.post('/', function(req, res, next){
+    return axios.post(COMMENTS_DATASOURCE_URL, req.body)
+    .then(response => {
+        return res.json(response.data);
+    }).catch(error => {
+        const status = error.response.status || 422;
+        const data = error.response.data || [];
+        return res.status(status).json(error.response.data);
     });
 });
 
