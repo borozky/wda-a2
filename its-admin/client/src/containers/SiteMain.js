0702087ba759e1a1a32fcc35f814a81b8babb8bf
Josh MacDev <joshmacdev@victoroscosiMac.gateway>
2017-09-28 00:16:01 +1000

    enables redux with react-router

--- its-admin/client/src/containers/SiteMain.js
+++ its-admin/client/src/containers/SiteMain.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
-import { Route } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 
 import DashboardPage from "./Dashboard/DashboardPage";
 import TicketsPage from "./Tickets/TicketsPage";
 import TicketPage from "./Tickets/TicketPage";
 import LeaderboardsPage from "./Leaderboards/LeaderboardsPage";
 
+import { connect } from "react-redux";
+import { withRouter } from 'react-router-dom'
+
 
 class SiteMain extends Component {
     render() {
+        console.log(this.props);
         return (
             <div className="site-main">
-                <Route exact path="/" component={DashboardPage} />
-                <Route path="/dashboard" component={DashboardPage} />
-                <Route exact path="/tickets" component={TicketsPage}/>
-                <Route exact path="/tickets/:id" component={TicketPage} />
-                <Route path="/leaderboards" component={LeaderboardsPage} />
+                <Switch>
+                    <Route exact path="/" component={DashboardPage} />
+                    <Route path="/dashboard" component={DashboardPage} />
+                    <Route exact path="/tickets" component={TicketsPage}/>
+                    <Route exact path="/tickets/:id" component={TicketPage} />
+                    <Route path="/leaderboards" component={LeaderboardsPage} />
+                </Switch>
             </div>
         );
     }
 }
 
-export default SiteMain;
+const mapStateToProps = (state) => {
+    return {
+        currentUser: state.session.currentUser
+    }
+}
+
+export default withRouter(connect(mapStateToProps)(SiteMain));
