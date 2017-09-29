13ee583d2a938c6dc838498ea81e3cd1ee6e18b4
Josh MacDev <joshmacdev@victoroscosiMac.gateway>
2017-09-29 10:12:10 +1000

    added sign in page design

--- its-admin/client/src/containers/SignInPage.js
+++ its-admin/client/src/containers/SignInPage.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import EntryHeader from "../components/EntryHeader";
 import { Route, Redirect } from "react-router-dom";
+import * as SessionActions from "../actions/SessionActions";
+import {connect} from "react-redux";
+import {withRouter} from "react-router-dom";
 
 class SignInPage extends Component {
+
     render() {
         return (
             <div className="site-main">
@@ -14,7 +18,11 @@ class SignInPage extends Component {
                             </h3>
                         </EntryHeader>
                         <div className="site-content">
-                            Sign in page
+                            <div className="container">
+                                <button className="btn btn-lg btn-primary" onClick={e => {this.props.onLogin()}}>
+                                    <i className="fa fa-facebook"></i> Login with Facebook
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </Route>
@@ -23,4 +31,12 @@ class SignInPage extends Component {
     }
 }
 
-export default SignInPage;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onLogin: function(){
+            dispatch(SessionActions.login());
+        }
+    }
+}
+
+export default withRouter(connect(null, mapDispatchToProps)(SignInPage));
