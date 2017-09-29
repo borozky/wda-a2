8f844264c1b15b0924b349cecfde88bd30225ecc
Josh MacDev <joshmacdev@victoroscosiMac.gateway>
2017-09-29 10:16:12 +1000

    added filters

--- its-admin/client/src/containers/Tickets/TicketsPage.js
+++ its-admin/client/src/containers/Tickets/TicketsPage.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import EntryHeader from "../../components/EntryHeader";
+import { FixedWidthSidebar, Sidebar, ResponsiveContent } from "../../components/FixedWidthSidebar";
 import faker from "faker";
 import { Redirect, Link } from "react-router-dom";
 import DataTable from "../../components/DataTable";
 import {connect} from "react-redux";
 import moment from "moment";
 import * as TicketActions from "../../actions/TicketActions";
+import TicketStatusBadge from "../../components/TicketStatusBadge";
+import TicketFilterForm from "./TicketFilterForm";
 
 class TicketsPage extends Component {
 
@@ -42,21 +45,30 @@ class TicketsPage extends Component {
             </EntryHeader>
             <div className="site-content">
                 <div className="container">
-                    <DataTable onSearch={this.handleOnSearch} loading={loading} data={tickets} columns={columns} style={{maxWidth: "100%"}}>{(ticket, index) => 
-                        <tr key={index}>
-                            <td>
-                                <div className="ticket-summary">
-                                    <Link to={`/tickets/${ticket.id}`}>
-                                        <b className="ticket-subject">{ticket.subject}</b>
-                                    </Link><br/>
-                                    <small className="ticket-meta">{ticket.software_issue} ({ticket.operating_system})</small>
-                                    <span className="ticket-id">#{ticket.id}</span>
-                                </div>
-                            </td>
-                            <td><span className="ticket-assigned_to"></span></td>
-                            <td><span className="ticket-created_at">{moment(ticket.created_at).format("DD/MM/YYYY").toString()}</span></td>
-                        </tr>
-                    }</DataTable>
+                    <FixedWidthSidebar direction="left">
+                        <Sidebar>
+                            <h4>Filter</h4>
+                            { this.props.filterSettings.map((setting, index) => <TicketFilterForm {...setting}/>)}
+                        </Sidebar>
+                        <ResponsiveContent style={{paddingRight: 20}}>
+                            <DataTable onSearch={this.handleOnSearch} loading={loading} data={tickets} columns={columns} style={{maxWidth: "100%"}}>{(ticket, index) => 
+                                <tr key={index}>
+                                    <td>
+                                        <div className="ticket-summary">
+                                            <Link to={`/tickets/${ticket.id}`}>
+                                                <b className="ticket-subject">{ticket.subject}</b>
+                                            </Link><br/>
+                                            <small className="ticket-meta">{ticket.software_issue} ({ticket.operating_system})</small>
+                                            <span className="ticket-id">#{ticket.id}</span>
+                                        </div>
+                                    </td>
+                                    <td><TicketStatusBadge status={ticket.status}/></td>
+                                    <td><span className="ticket-assigned-to"></span></td>
+                                    <td><span className="ticket-created_at">{moment(ticket.created_at).format("DD/MM/YYYY").toString()}</span></td>
+                                </tr>
+                            }</DataTable>
+                        </ResponsiveContent>
+                    </FixedWidthSidebar>
                 </div>
             </div>
         </div>
@@ -67,8 +79,79 @@ class TicketsPage extends Component {
 const mapStateToProps = (state) => {
     return {
         tickets: state.tickets.foundTickets,
-        columns: ["Tickets", "Assigned to", "Created"],
-        loading: state.tickets.loading
+        columns: ["Tickets", "Status", "Assigned to", "Created"],
+        loading: state.tickets.loading,
+        filterSettings: [
+            {
+                title: "Ticket status",
+                name: "ticket-status",
+                filters: [
+                    {
+                        label: "Pending",
+                        defaultValue: "Pending",
+                        id: "TicketStatus-Pending"
+                    },
+                    {
+                        label: "In Progress",
+                        defaultValue: "In Progress",
+                        id: "TicketStatus-In_Progress"
+                    },
+                    {
+                        label: "Unresolved",
+                        defaultValue: "Unresolved",
+                        id: "TicketStatus-Unresolved"
+                    },
+                    {
+                        label: "Resolved",
+                        defaultValue: "Resolved",
+                        id: "TicketStatus-Resolved"
+                    }
+                ]
+            },
+            {
+                title: "Escalation",
+                name: "ticket-escalation",
+                filters: [
+                    {
+                        label: "Level 1",
+                        defaultValue: 1,
+                        id: "TicketEscalationLevel1"
+                    },
+                    {
+                        label: "Level 2",
+                        defaultValue: 2,
+                        id: "TicketEscalationLevel2"
+                    },
+                    {
+                        label: "Level 3",
+                        defaultValue: 3,
+                        id: "TicketEscalationLevel3"
+                    },
+                ]
+            },
+            {
+                title: "Priority",
+                name: "ticket-priority",
+                filters: [
+                    {
+                        label: "Low",
+                        defaultValue: "low",
+                        id: "TicketPriority-Low"
+                    },
+                    {
+                        label: "Medium",
+                        defaultValue: "medium",
+                        id: "TicketPriority-Medium"
+                    },
+                    {
+                        label: "High",
+                        defaultValue: "high",
+                        id: "TicketPriority-High"
+                    },
+                ]
+            },
+            
+        ]
     }
 }
 
